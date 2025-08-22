912e923dc7027d6f0307e2e5ec335f4816fe9f4c
Krishna Varma <kvarma00011@gmail.com>
2025-08-22 15:26:58 +0530

    chore: add Vercel SPA rewrite, Admin page, and ignore .env

---
+++ src/pages/Admin.tsx
@@ -0,0 +1,9 @@
+import AdminLogin from "@/components/admin/AdminLogin";
+
+export default function Admin() {
+  return (
+    <div className="container mx-auto p-4">
+      <AdminLogin />
+    </div>
+  );
+}
